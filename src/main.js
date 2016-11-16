2499ee7b89abb342de8190ee53c9813a307a2fc9
Sergey Panfilov <se-panfilov@ya.ru>
2016-11-16 13:32:47 +0100

    fixes for vuex (added password field)

--- src/main.js
+++ src/main.js
@@ -148,6 +148,8 @@ const innerMethods = {
       // const interval = setInterval(() => {
       let prev
       let cur
+
+      // TODO (S.Panfilov)make sure no memory leak here, destroy interval when we're leave page
       setInterval(() => {
         if (watch) {
           cur = watch()
