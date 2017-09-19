856835d5db632d2d6f99baaebbaef946f7fc780d
Sergei <se-panfilov@ya.ru>
2017-09-19 21:40:02 +0800

    initial website commit

--- src/App.vue
+++ src/App.vue
@@ -1,121 +1,23 @@
 <template>
   <div id="app">
-
-    <section>
-      Check the <a href="docs/html/index.html">the Documentation here</a>
-      <br/>
-    </section>
-
-    <section>
-      <h5>Predefined messages</h5>
-      <button type="button" @click="errorMsg()">Error</button>
-      <button type="button" @click="warnMsg()">Warning</button>
-      <button type="button" @click="infoMsg()">Info</button>
-      <button type="button" @click="successMsg()">Success</button>
-    </section>
-
-    <section>
-      <h5>Dynamic messages</h5>
-      <label> Title:
-        <input type="text" v-model="title">
-      </label>
-      <label> Message:
-        <textarea v-model="msg"></textarea>
-      </label>
-      <div>
-        <div v-for="t in types">
-          <label>
-            <input type="radio" name="type" :value="t" v-model="type"/>
-            <span v-text="t"></span>
-          </label>
-        </div>
-      </div>
-      <div>
-        <button type="button" @click="dynamicMsg()">Success</button>
-      </div>
-    </section>
-
+    <img src="./assets/logo.png">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-  export default {
-    name: 'app',
-    data () {
-      return {
-        switchVal: true,
-        types: [
-          'error',
-          'warn',
-          'info',
-          'success',
-        ],
-        title: 'Your title',
-        msg: 'Your message',
-        type: 'error'
-      }
-    },
-    created () {
-      this.errorMsg()
-      this.warnMsg()
-      this.infoMsg()
-      this.successMsg()
-    },
-    methods: {
-      switchLinkedVal () {
-        this.switchVal = !this.switchVal
-//        console.info(this.switchVal)
-      }
-    },
-    notifications: {
-      errorMsg: {
-        type: 'error',
-        title: 'Error title',
-        message: 'Some error msg'
-      },
-      warnMsg: {
-        type: 'warn',
-        title: 'Warning title',
-        message: 'Some warn msg'
-      },
-      infoMsg: {
-        type: 'info',
-        title: 'Info title',
-        message: 'Some info msg'
-      },
-      successMsg: {
-        type: 'success',
-        title: 'Success title',
-        message: 'Some success msg'
-      },
-      dynamicMsg: {
-        type () {
-          return this.type
-        },
-        title () {
-          return this.title
-        },
-        message () {
-          return this.msg
-        }
-      }
-    }
-  }
+export default {
+  name: 'app'
+}
 </script>
 
 <style>
-
-  .base_msg {
-    display: block;
-    min-height: 20px;
-    background-color: #0b97c4;
-  }
-
-  .-in_class {
-    background-color: red;
-  }
-
-  .-out_class {
-    background-color: green;
-  }
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,31 +1,15 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
-import App from './App.vue'
-import VueNotifications from 'vue-notifications'
-import miniToastr from 'mini-toastr'
+import App from './App'
+import router from './router'
 
-miniToastr.init()
+Vue.config.productionTip = false
 
-function toast ({title, message, type, timeout, cb, debugMsg}) {
-  if (debugMsg) console[type](debugMsg)
-  return miniToastr[type](message, title, timeout, cb)
-}
-
-const options = {
-  success: toast,
-  error: toast,
-  info: toast,
-  warn: toast
-}
-
-Vue.use(VueNotifications, options)
-// Vue.use(VueNotifications)
-
+/* eslint-disable no-new */
 new Vue({
   el: '#app',
-  render: h => h(App),
-  created: function () {
-    // `this` points to the vm instance
-    // VueNotifications.error({message: 'asdsad'})
-    // console.log(VueNotifications)
-  }
+  router,
+  template: '<App/>',
+  components: { App }
 })
