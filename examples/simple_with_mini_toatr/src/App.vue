9d19f05a323b7d3ee5371bcbb950b33f44fb1be0
Sergei <se-panfilov@ya.ru>
2017-09-30 22:42:01 +0800

    added first example

---
+++ examples/simple_with_mini_toatr/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <header-menu></header-menu>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  import HeaderMenu from './components/HeaderMenu'
+  export default {
+    name: 'app',
+    components: {
+      HeaderMenu
+    }
+  }
+</script>
+
+<style lang="stylus" type="text/stylus">
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    -webkit-font-smoothing antialiased
+    -moz-osx-font-smoothing grayscale
+    color #2c3e50
+    margin-top 110px
+</style>
---
+++ examples/simple_with_mini_toatr/src/components/Hello.vue
@@ -0,0 +1,82 @@
+<template>
+  <div class="hello">
+    <h1>VueNotifications</h1>
+    <img src="../assets/logo.png">
+    <h2><a href="https://vuejs.org">vue.js</a> agnostic non-blocking notifications library</h2>
+    <ul class="msg-buttons">
+      <li>
+        <button type="button" class="msg-buttons__btn -success" @click="showSuccessMsg()">Success</button>
+      </li>
+      <li>
+        <button type="button" class="msg-buttons__btn -info" @click="showInfoMsg()">Info</button>
+      </li>
+      <li>
+        <button type="button" class="msg-buttons__btn -warn" @click="showWarnMsg()">Warning</button>
+      </li>
+      <li>
+        <button type="button" class="msg-buttons__btn -error" @click="showErrorMsg()">Error</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import VueNotifications from 'vue-notifications'
+  export default {
+    name: 'hello',
+    data () {
+      return {}
+    },
+    notifications: {
+      showSuccessMsg: {
+        type: VueNotifications.type.success,
+        title: 'Hello there',
+        message: 'That\'s the success!'
+      },
+      showInfoMsg: {
+        type: VueNotifications.type.info,
+        title: 'Hey you',
+        message: 'Here is some info for you'
+      },
+      showWarnMsg: {
+        type: VueNotifications.type.warn,
+        title: 'Wow, man',
+        message: 'That\'s the kind of warning'
+      },
+      showErrorMsg: {
+        type: VueNotifications.type.error,
+        title: 'Wow-wow',
+        message: 'That\'s the error'
+      }
+    }
+  }
+</script>
+
+<style lang="stylus" type="text/stylus" scoped>
+  primary_color = #41b883
+  h1, h2
+    font-weight normal
+  ul
+    list-style-type none
+    padding 0
+  li
+    display inline-block
+    margin 0 10px
+  a
+    color #42b983
+  .hello
+    text-align center
+  .msg-buttons
+    margin 10px
+    &__btn
+      color white
+      font-size 14px
+      background-color primary_color
+      border-radius 3px
+      padding 7px 12px
+      border 1px solid transparent
+      transition background .4s ease
+      cursor pointer
+      &:hover
+        background-color lighten(primary_color, 5)
+</style>
