100db93fcc42a26659c6ee9ac96d37f53c635388
Sergei Panfilov <sergei.panfilov@powerhouse.nl>
2018-04-05 15:16:21 +0200

    init typescript

---
+++ src/message.constant.ts
@@ -0,0 +1,4 @@
+import {PLUGIN_NAME} from './package.constant'
+
+export const ALREADY_INSTALLED: string = `${PLUGIN_NAME}: plugin already installed`
+export const METHOD_NAME_CONFLICT: string = `${PLUGIN_NAME}: names conflict - `
