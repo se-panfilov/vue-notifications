5c98bbd1aac3bf26bb0d652e76fee13f6c845059
Sergei Panfilov <se-panfilov@ya.ru>
2019-08-01 14:27:18 +0200

    updated deps

--- dist/vue-notifications.d.ts
+++ dist/vue-notifications.d.ts
@@ -2,35 +2,32 @@ import { ComponentOptions, PluginObject, VueConstructor } from 'vue'
 import { Vue } from 'vue/types/vue'
 
 declare enum DEFAULT_MESSAGE_TYPES {
-  error = 'error',
-  warn = 'warn',
-  info = 'info',
-  success = 'success'
+    error = "error",
+    warn = "warn",
+    info = "info",
+    success = "success"
 }
-
-declare const VueNotifications: VueNotificationsPlugin
+declare const VueNotifications: VueNotificationsPlugin;
 export interface NotificationsObject {
-  readonly [key: string]: MessageData;
+    readonly [key: string]: MessageData;
 }
 export interface MessageData {
-  type: DEFAULT_MESSAGE_TYPES | string;
-  timeout?: number;
-  message?: string;
-  title?: string;
-  cb?: () => any;
-
-  [key: string]: any;
+    type: DEFAULT_MESSAGE_TYPES | string;
+    timeout?: number;
+    message?: string;
+    title?: string;
+    cb?: () => any;
+    [key: string]: any;
 }
 export interface VueNotificationsPlugin extends PluginObject<any> {
-  types: {
-    [key: string]: DEFAULT_MESSAGE_TYPES | string;
-  };
-  propertyName: string;
-  config: MessageData;
-  pluginOptions: ComponentOptions<Vue>;
-  installed: boolean;
-  install: (vue: VueConstructor, pluginOptions: ComponentOptions<Vue>) => void;
-  setPluginOptions: (options: ComponentOptions<Vue>) => void;
+    types: {
+        [key: string]: DEFAULT_MESSAGE_TYPES | string;
+    };
+    propertyName: string;
+    config: MessageData;
+    pluginOptions: ComponentOptions<Vue>;
+    installed: boolean;
+    install: (vue: VueConstructor, pluginOptions: ComponentOptions<Vue>) => void;
+    setPluginOptions: (options: ComponentOptions<Vue>) => void;
 }
-
-export default VueNotifications
+export default VueNotifications;
